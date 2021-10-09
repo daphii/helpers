80f702a2831c0de532715c06d937cf45be0cf5b3
daphii <74498583+daphii@users.noreply.github.com>
2021-10-09 12:04:04 -0400

    script timer

---
+++ codeTimer.py
@@ -0,0 +1,20 @@
+import timeit
+
+mysetup = ""
+
+mycode = ''''''
+
+mycode2 = ''''''
+
+numOfExecutions = 10000
+
+time1 = timeit.timeit(setup = mysetup,
+                     stmt = mycode,
+                     number = numOfExecutions)
+
+time2 = timeit.timeit(setup = mysetup,
+                     stmt = mycode2,
+                     number = numOfExecutions)
+
+print("\n{} Executions\nCode 1 : {}\nCode 2: {}".format(numOfExecutions, time1, time2))
+
